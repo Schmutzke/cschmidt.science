a48f124c1c89dec33a3434872064fc8f6508f8d9
Christian Schmidt <christianschmidt@uvic.ca>
2026-03-13 10:32:07 -0700

    Add breadcrumb home link to content pages

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -19,6 +19,10 @@ export const sharedPageComponents: SharedLayout = {
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.ConditionalRender({
+      component: Component.Breadcrumbs(),
+      condition: (page) => page.fileData.slug !== "index",
+    }),
+    Component.ConditionalRender({
       component: Component.ArticleTitle(),
       condition: (page) => page.fileData.slug !== "index",
     }),
